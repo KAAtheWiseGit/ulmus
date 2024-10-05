8f7ebe200d96b914ce8764b6288a071da3538751
Andrej Kolčin <self@kaathewise.net>
2024-10-05 17:43:25 +0300

    [src] interface: add a command type

--- src/interface.rs
+++ src/interface.rs
@@ -1,8 +1,20 @@
-use crossterm::event::Event as CrosstermEvent;
+use crossterm::{event::Event as CrosstermEvent, Command as CrosstermCommand};
+
+use std::sync::mpsc;
+
+type Subroutine<T> = fn(mpsc::Sender<Msg<T>>);
 
 pub enum Msg<T> {
 	Term(CrosstermEvent),
 	Custom(T),
+}
+
+pub enum Cmd<T> {
+	// TODO implement an opaque type, which can supports From for crossterm
+	// commands
+	Term,
+	Quit,
+	Subroutine(Subroutine<T>),
 }
 
 pub trait Model: Sized {
@@ -10,7 +22,10 @@ pub trait Model: Sized {
 
 	fn init() -> Self;
 
-	fn update(&mut self, message: Msg<Self::CustomMsg>);
+	fn update(
+		&mut self,
+		message: Msg<Self::CustomMsg>,
+	) -> Cmd<Self::CustomMsg>;
 
 	fn view(&self) -> impl AsRef<str>;
 }
