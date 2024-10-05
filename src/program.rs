5b2350c5c82fef3d9f2256b9d1913c3ef1e19e8d
Andrej Kolčin <self@kaathewise.net>
2024-10-05 17:27:57 +0300

    [src] program: add run loop stub

--- src/program.rs
+++ src/program.rs
@@ -1,4 +1,12 @@
-use std::sync::mpsc;
+use crossterm::{
+	terminal::{
+		disable_raw_mode, enable_raw_mode, Clear, ClearType,
+		EnterAlternateScreen, LeaveAlternateScreen,
+	},
+	ExecutableCommand,
+};
+
+use std::{io::stdout, sync::mpsc};
 
 use crate::interface::Msg;
 
@@ -10,4 +18,33 @@ where
 	model: M,
 	reciever: mpsc::Receiver<Msg<T>>,
 	sender: mpsc::Sender<Msg<T>>,
+}
+
+impl<M, T> Program<M, T>
+where
+	M: crate::Model<CustomMsg = T>,
+	T: Sized,
+{
+	pub fn new(model: M) -> Self {
+		let (sender, reciever) = mpsc::channel::<Msg<T>>();
+
+		Self {
+			model,
+			reciever,
+			sender,
+		}
+	}
+
+	pub fn run(&mut self) {
+		let mut stdout = stdout();
+
+		// Setup the TUI view
+		stdout.execute(EnterAlternateScreen);
+		enable_raw_mode().unwrap();
+		stdout.execute(Clear(ClearType::All));
+
+		// Restore the terminal view
+		disable_raw_mode().unwrap();
+		stdout.execute(LeaveAlternateScreen);
+	}
 }
