26b247d5f4e22b8ca56430f5ab243fd5ca547c2f
Andrej Kolčin <self@kaathewise.net>
2024-10-07 18:45:44 +0300

    [src] view: delete module

--- src/lib.rs
+++ src/lib.rs
@@ -3,10 +3,8 @@
 mod model;
 mod program;
 mod reactive;
-mod view;
 pub mod widget;
 
 pub use model::Model;
 pub use program::Program;
 pub use reactive::{Cmd, Msg, Reactive, Subroutine, TermCommand};
-pub use view::View;
