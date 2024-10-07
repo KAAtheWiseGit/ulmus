2132216e1431ef2355dc15393320ec3123376efc
Andrej Kolčin <self@kaathewise.net>
2024-10-07 18:47:01 +0300

    [src] model: replace `View` constraint with a method

--- src/model.rs
+++ src/model.rs
@@ -1,8 +1,10 @@
-use crate::{Cmd, Reactive, View};
+use crate::{Cmd, Reactive};
 
 /// The `Model` trait describes the behaviour of your TUI.
-pub trait Model: Reactive + View {
+pub trait Model: Reactive {
 	/// Returns [commands][`Cmd`] which will be ran on startup, before the
 	/// first render.
 	fn init(&self) -> Vec<Cmd<Self::CustomMsg>>;
+
+	fn view(&self) -> impl crossterm::Command;
 }
