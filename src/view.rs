3348f31456db099c9de319ad0ba9ad970eff824b
Andrej Kolčin <self@kaathewise.net>
2024-10-07 18:29:31 +0300

    [src] view: fix string rendering

--- src/view.rs
+++ src/view.rs
@@ -18,7 +18,7 @@ impl<T: AsRef<str>> IntoCommand for T {
 	type IntoCommand<'a> = StrView<'a> where Self: 'a;
 
 	fn into_command(&self) -> Self::IntoCommand<'_> {
-		todo!()
+		StrView::from(self.as_ref())
 	}
 }
 
@@ -26,13 +26,23 @@ pub struct StrView<'a> {
 	source: &'a str,
 }
 
+impl<'a> StrView<'a> {
+	fn from(source: &'a str) -> StrView<'a> {
+		Self { source }
+	}
+}
+
 impl crossterm::Command for StrView<'_> {
 	fn write_ansi(&self, f: &mut impl Write) -> fmt::Result {
 		cursor::SavePosition.write_ansi(f)?;
 
-		for line in self.source.lines() {
+		for (i, line) in self.source.lines().enumerate() {
+			cursor::SavePosition.write_ansi(f)?;
+			if i > 0 {
+				cursor::MoveDown(i as u16).write_ansi(f)?;
+			}
 			f.write_str(line)?;
-			cursor::MoveDown(1).write_ansi(f)?;
+			cursor::RestorePosition.write_ansi(f)?;
 		}
 
 		cursor::RestorePosition.write_ansi(f)?;
