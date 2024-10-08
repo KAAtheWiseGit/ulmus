84dc9f00fa97a0f20cca55979e77f6fe5df24346
Andrej Kolčin <self@kaathewise.net>
2024-10-08 18:01:35 +0300

    [src] reactive: add methods for working with `Message`

--- src/reactive.rs
+++ src/reactive.rs
@@ -8,6 +8,26 @@ pub struct Message {
 	value: Box<dyn Any + Send>,
 }
 
+impl Message {
+	pub fn new<T: 'static + Send>(value: T) -> Self {
+		Message {
+			value: Box::new(value),
+		}
+	}
+
+	pub fn as_ref<T: 'static + Send>(&self) -> Option<&T> {
+		self.value.downcast_ref()
+	}
+
+	pub fn downcast<T: 'static + Send>(self) -> Result<Box<T>, Self> {
+		self.value.downcast::<T>().map_err(|value| Self { value })
+	}
+
+	pub fn is<T: 'static + Send>(&self) -> bool {
+		self.value.is::<T>()
+	}
+}
+
 /// Commands are returned by model in [`init`][Model#tymethod.init] and
 /// [`update`][Model#tymethod.update] and can be used to change control the
 /// event loop.
