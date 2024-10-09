e677ddeec3869e0be0d11a93b267c5adde523b03
Andrej Kolčin <self@kaathewise.net>
2024-10-09 12:05:08 +0300

    [src] reactive: delete module
    With the new widget architecture I use, they don't need to be reactive.
    Thus, only the model needs this method, meaning we can roll it back into
    `Model`.

--- src/lib.rs
+++ src/lib.rs
@@ -2,11 +2,9 @@
 
 mod model;
 mod program;
-mod reactive;
 mod view;
 pub mod widget;
 
-pub use model::Model;
+pub use model::{Command, Message, Model, Subroutine};
 pub use program::Program;
-pub use reactive::{Command, Message, Reactive, Subroutine};
 pub use view::{Area, View};
--- src/model.rs
+++ src/model.rs
@@ -1,8 +1,50 @@
-use crate::{Command, Reactive, View};
+use std::{any::Any, sync::mpsc};
+
+use crate::View;
+
+pub type Subroutine = Box<dyn FnOnce(mpsc::Sender<Message>) + Send>;
+
+pub struct Message {
+	value: Box<dyn Any + Send>,
+}
+
+impl Message {
+	pub fn new<T: 'static + Send>(value: T) -> Self {
+		Message {
+			value: Box::new(value),
+		}
+	}
+
+	pub fn as_ref<T: 'static + Send>(&self) -> Option<&T> {
+		self.value.downcast_ref()
+	}
+
+	pub fn downcast<T: 'static + Send>(self) -> Result<Box<T>, Self> {
+		self.value.downcast::<T>().map_err(|value| Self { value })
+	}
+
+	pub fn is<T: 'static + Send>(&self) -> bool {
+		self.value.is::<T>()
+	}
+}
+
+/// Commands are returned by model in [`init`][Model#tymethod.init] and
+/// [`update`][Model#tymethod.update] and can be used to change control the
+/// event loop.
+pub enum Command {
+	/// Immediately shuts down the program.
+	Quit,
+	/// Launches a subroutine.  Note that this command can be sent at any
+	/// time, even in [`update`][Model#tymethod.update] and that the
+	/// subroutine can start sending messages immediately.
+	Subroutine(Subroutine),
+}
 
 /// The `Model` trait describes the behaviour of your TUI.
-pub trait Model: Reactive + View {
+pub trait Model: View {
 	/// Returns [commands][`Cmd`] which will be ran on startup, before the
 	/// first render.
 	fn init(&self) -> Vec<Command>;
+
+	fn update(&mut self, message: Message) -> Vec<Command>;
 }
