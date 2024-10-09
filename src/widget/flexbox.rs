6674a756439bc165993491f29d5cd3af1dcbd653
Andrej Kolčin <self@kaathewise.net>
2024-10-09 13:43:22 +0300

    [src] widget/flexbox: add layout method

--- src/widget/flexbox.rs
+++ src/widget/flexbox.rs
@@ -8,10 +8,11 @@ pub enum Direction {
 	Horizontal,
 }
 
+#[derive(PartialEq, Eq, PartialOrd, Ord, Clone, Copy)]
 pub enum Size {
-	Length(usize),
+	Length(u16),
 	Auto,
-	Fraction(usize),
+	Fraction(u16),
 }
 
 pub struct Flexbox {
@@ -48,6 +49,49 @@ impl Flexbox {
 		sizes: Vec<Size>,
 	) -> Box<Flexbox> {
 		Flexbox::new(Direction::Horizontal, widgets, sizes)
+	}
+
+	fn layout(&self, direction: Direction, length: u16) -> Vec<u16> {
+		let mut out = vec![0; self.widgets.len()];
+		let mut total_length = length;
+
+		macro_rules! cut {
+			($i:ident, $length:expr) => {
+				if total_length >= $length {
+					total_length -= $length;
+					out[$i] = $length;
+				} else {
+					out[$i] = total_length;
+					return out;
+				}
+			};
+		}
+
+		for (i, s) in self.sizes.iter().enumerate() {
+			if let Size::Length(size_length) = s {
+				cut!(i, *size_length);
+			}
+		}
+
+		for (i, s) in self.sizes.iter().enumerate() {
+			if let Size::Auto = s {
+				let hint_length = match direction {
+					Direction::Vertical => self.widgets[i]
+						.get_width_hint()
+						as u16,
+					Direction::Horizontal => {
+						self.widgets[i]
+							.get_height_hint() as u16
+					}
+				};
+
+				cut!(i, hint_length);
+			}
+		}
+
+		// TODO: fractions
+
+		out
 	}
 }
 
